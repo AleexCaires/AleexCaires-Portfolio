b78fedc99fad8a279ef38c45cb54d45fdc2c7754
AleexCaires <tiagoaleex98@gmail.com>
2020-10-28 20:48:08 +0000

    feat: Mario helped….I watched
    Hero animation with a type style from a given array of words

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,32 +5,68 @@ import HeroStyles from "../styles/components/hero.module.scss"
 class Hero extends React.Component {
 
     componentDidMount() {
-        var text = document.getElementById('text');
-        var word = text.getElementsByTagName('span');
-        var i = 0;
+        const phrases = ["Web Developer", "Front End Developer","Technology Lover", "Aspiring Fullstack Developer", "Freelancer"];
+        const text = document.getElementById('text');
+        let word = 0;
+        let letter = 0;
+        // Direction true writes a new word
+        // Direction false erases existing word
+        let direction = false;
 
-        function rotator(){
-	        word[i].style.display= 'none';
-	        i = (i + 1) % word.length;
-	        word[i].style.display = 'initial';
-}
-setInterval(rotator, 1500);
+        function changeWord(){
+            word > (phrases.length - 1) ? word = 0 : //null ;
+            text.innerText = phrases[word].slice(0, -(phrases[word].length - 1));
+        }
+
+        function wordAnimation() {
+
+            setTimeout(()=>{
+                if (direction) {
+                    if ( letter <= phrases[word].length ) {
+                        if ( letter !== 0 ) {
+                            //console.log(phrases[word].slice(0, letter));
+                            text.innerText = String(phrases[word]).slice(0, letter);
+                        }
+                        letter++;
+                        wordAnimation();
+                    } else {
+                        letter = 0;
+                        direction = false;
+                        wordAnimation();
+                    }
+                } else {
+                    if ( letter <= phrases[word].length ) {
+                        if ( letter !== 0 ) {
+                            //console.log(phrases[word].slice(0, letter));
+                            text.innerText = String(phrases[word]).slice(0, -letter);
+                        }
+                        letter++;
+                        wordAnimation();
+                    } else {
+                        word++;
+                        letter = 0;
+                        changeWord();
+                        direction = true;
+                        wordAnimation();
+                    }
+                }
+            }, 200);
+
+        }
+
+        wordAnimation();
+
     }
 
         render() {
             return(
-    <div id={HeroStyles.hero}>
-        <div className={HeroStyles.inner}>
-            <h1>Hello,</h1>
-            <h2 id="text">I am Tiago Caires a <br></br>
-                <span>Web Developer</span>
-	            <span>Front End Developer</span>
-                <span>Banner Developer</span>
-	            <span>Freelancer</span> &#8287;from London.
-            </h2>
-            <a class="btn" href="https://google.com" target="_blank" rel="noreferrer">Learn More</a>
-        </div>
-    </div>
+                <div id={HeroStyles.hero}>
+                    <div className={HeroStyles.inner}>
+                        <h1>Hello,</h1>
+                        <h2>I am Tiago Caires a<br/><span id="text">Web Developer</span> from London.</h2>
+                        <a className="btn" href="https://google.com" target="_blank" rel="noreferrer">Learn More</a>
+                    </div>
+                </div>
             )
         }
 }
