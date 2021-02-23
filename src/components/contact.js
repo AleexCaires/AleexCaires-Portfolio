5518dbac18eb5fc2c7c8270bde00510014046e37
AleexCaires <tiagoaleex98@gmail.com>
2021-02-23 12:26:40 +0000

    contact page update

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,17 +4,6 @@ import React from "react";
 const Contact = () => (
     <div id={ContactStyles.main}> 
         <h1>Contact Form</h1>
-        <p>If you want to get in touch to discuss about an amazing project opportunity, job role, consultancy,
-            tuition or if you just want<br></br> to say "Hi", feel free to contact me via using one of the options bellow.></p>
-            <ul>
-                <li>
-                    LinkedIn 
-                    <a href="https://www.linkedin.com/in/alex-caires-b7b39b174/" rel="noopener noreferrer" target="_blank">@TiagoCaires</a>
-                </li>
-            </ul>
-            <p>Or using the form bellow.</p>
-
-
             <form action="">
                 <input type="text" placeholder="Your full Name.." name="name" required></input>
                 <input type="email" placeholder="Your Email address.." name="email" required></input>
