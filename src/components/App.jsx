5366332f46080a6946fbe165c84aafba00239aa0
AleexCaires <tiagoaleex98@gmail.com>
2021-07-13 00:42:51 +0100

    Added FeaturedClients Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import About from './About/About';
 import Projects from './Projects/Projects';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
+import FeaturedClients from './Featured Clients/FeaturedClients';
 
 import { PortfolioProvider } from '../context/context';
 
@@ -29,9 +30,11 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <FeaturedClients />
       <Contact />
       <Footer />
-    </PortfolioProvider>
+      
+      </PortfolioProvider>
   );
 }
 
